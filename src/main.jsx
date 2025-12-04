1c0d7e68979d5ba189eff341a9e8654bc308c9e6
scottybee84 <84326190+scottybee84@users.noreply.github.com>
2025-12-04 06:13:48 -0600

    Remove basename from BrowserRouter to simplify routing configuration

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/niche-validation-test">
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/agentos" replace />} />
         <Route path="/agentos" element={<AgentOS />} />
